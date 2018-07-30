709641072584ed2c1f3a4b5eaad13bf8693bb961
chunmu.zhang <duuuuuke@sina.com>
2018-07-30 22:58:40 +0800

    init project

---
+++ src/common/config/database.js
@@ -0,0 +1,13 @@
+const mysql = require('think-model-mysql');
+const isDev = think.env === 'development';
+module.exports = {
+  handle: mysql,
+  database: 'wetrip',
+  prefix: 'wetrip_',
+  encoding: 'utf8mb4',
+  host: isDev ? '127.0.0.1' : 'obbedutnxoxu.mysql.sae.sina.com.cn',
+  port: isDev ? '3306' : '10215',
+  user: isDev ? 'root' : 'wetrip',
+  password: isDev ? 'Zcm199335' : '',
+  dateStrings: true
+};
